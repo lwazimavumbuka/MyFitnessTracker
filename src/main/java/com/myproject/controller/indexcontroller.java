399b9e0e9864a2456c2b1b2d93ec3e9c28a38942
Lwazi Mavumbuka <mvmlwa003@myuct.ac.za>
2024-10-23 01:01:55 +0200

    navigation bar

---
+++ src/main/java/com/myproject/controller/indexcontroller.java
@@ -0,0 +1,14 @@
+package com.myproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class indexcontroller {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
