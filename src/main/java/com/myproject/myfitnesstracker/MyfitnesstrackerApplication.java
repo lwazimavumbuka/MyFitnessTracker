687cd5d49e390a1ead2a5d7b5718b3d6670dae8f
Lwazi Mavumbuka <mvmlwa003@myuct.ac.za>
2024-10-22 11:29:27 +0200

    First Commit

---
+++ src/main/java/com/myproject/myfitnesstracker/MyfitnesstrackerApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.myfitnesstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfitnesstrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfitnesstrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/myfitnesstracker/MyfitnesstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.myfitnesstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfitnesstrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
